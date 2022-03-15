31bd3c2a6c1462e21174971db7f05b0fe3af94fb
Andrew Medvedskiy <a.v.medvedskiy@gmail.com>
2022-03-15 11:16:30 +0200

    add scripts to make screen click tools/capturescreenshot

---
+++ Editor/ScreenshotTool.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace ScreenshotTool.Editor
+{
+    public class ScreenshotTool : EditorWindow
+    {
+
+        [MenuItem("Tools/Capture Screenshot")]
+        static void CaptureScreenshot()
+        {
+            ScreenCapture.CaptureScreenshot($"screenshot_{DateTime.Now:HH_mm_ss}.png", 1);
+        }
+    }
+}
